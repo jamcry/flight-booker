3b2a520e15f3aa8ef343bf819305663d0af027ad
jamcry <jamcry@hotmail.com>
2019-05-27 21:14:07 +0300

    Refactor indentation

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,4 @@
 class Airport < ApplicationRecord
-  has_many :arriving_flights, class_name: "Flight", foreign_key: :to_id
+  has_many :arriving_flights,  class_name: "Flight", foreign_key: :to_id
   has_many :departing_flights, class_name: "Flight", foreign_key: :from_id
 end
