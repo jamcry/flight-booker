2a50bf78d8750b12c84722971267d2fdce49420d
jamcry <jamcry@hotmail.com>
2019-06-01 11:16:17 +0300

    Add about page with static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'flights#index'
+  get 'about', to: 'static_pages#about'
   get 'bookings/new'
   get 'bookings/show'
-  root 'flights#index'
+  
   resources :flights
   resources :bookings
 end
