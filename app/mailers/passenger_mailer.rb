f15d18728c8ae5b5d0aef6eb60cb91b55682ce03
jamcry <jamcry@hotmail.com>
2019-06-06 02:59:30 +0300

    Create booking_email action and views

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+	default from: 'notifications@flightbooker.com'
+
+	def booking_email
+		@passenger = params[:passenger]
+		@booking = params[:booking]
+		mail(to: @user.email, subject: "Your booking confirmation")
+	end
 end
